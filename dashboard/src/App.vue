9f6443c1acdc05a926cfa68e8b56a0c86fc1d7fe
Oz Ramos <heyozramos@gmail.com>
2019-12-29 14:12:36 -0800

    #23 Starts tab management card

--- dashboard/src/App.vue
+++ dashboard/src/App.vue
@@ -2,13 +2,20 @@
   .panel.pa-3.full-height
     .panel-body
       .input-group
-        input.form-input.input-lg(type='text' placeholder='URL or web search')
-        button.btn.btn-primary.input-group-btn.btn-lg Submit
+        input.form-input.input-xl(type='text' placeholder='URL or web search')
+        button.btn.btn-primary.input-group-btn.btn-xl(style='width: 150px') Submit
+
+      .columns.mt-3
+        .column.col-4
+          TabManagementCard
 </template>
 
 <script>
+import TabManagementCard from '@/components/TabManagementCard'
+
 export default {
-  name: 'app'
+  name: 'app',
+  components: { TabManagementCard }
 }
 </script>
 
---
+++ dashboard/src/components/TabManagementCard.vue
@@ -0,0 +1,18 @@
+<template lang="pug">
+.card
+  .card-header
+    .card-title.h5 Tab Management
+  .card-body
+    p
+      button.btn.btn-primary.block.btn-xl 👈 Prev Tab
+    p
+      button.btn.btn-primary.block.btn-xl 👉 Next Tab
+    p
+      button.btn.btn-primary.block.btn-xl ➕ New Tab
+    p
+      button.btn.btn-primary.block.btn-xl ❌ Close Tab
+</template>
+
+<script>
+export default {}
+</script>
