3c81f3e89fcd9ae5ceef8bb9e345d8c460863258
Oz Ramos <heyozramos@gmail.com>
2019-12-30 14:39:23 -0800

    #25 Adds calibration button

--- dashboard/src/views/Calibrate.vue
+++ dashboard/src/views/Calibrate.vue
@@ -1,7 +1,54 @@
 <template lang="pug">
-  h1 Calibrate
+  .container.mt-3
+    h1 Calibrate
+    p Click the calibration button below and then point your head at the calibration dot that appears below it. The pointer will readjust itself until it reaches the calibration dot and hovers over it for 3 seconds.
+
+    .text-center
+      p(v-if='!isCalibrating && !isAboutToCalibrate')
+        button.btn.btn-primary.btn-xl(@click='startCalibration') Start calibration
+      p(v-if='isAboutToCalibrate || isCalibrating')
+        button.btn.btn-primary.btn-xl(disabled) Point head towards circle below
+      #calibration-marker(v-if='isCalibrating || isAboutToCalibrate')
 </template>
 
 <script>
-export default {}
+export default {
+  data: () => ({
+    isCalibrating: false,
+    isAboutToCalibrate: false
+  }),
+
+  methods: {
+    /**
+     * Starts the calibration process
+     */
+    startCalibration() {
+      this.isAboutToCalibrate = true
+      setTimeout(() => {
+        this.isAboutToCalibrate = false
+        this.isCalibrating = true
+
+        chrome &&
+          chrome.runtime.sendMessage &&
+          chrome.runtime.sendMessage({
+            action: 'startCalibration',
+            center: {
+              x: 0,
+              y: 0
+            }
+          })
+      }, 2000)
+    }
+  }
+}
 </script>
+
+<style scoped>
+#calibration-marker {
+  width: 160px;
+  height: 160px;
+  border-radius: 160px;
+  margin: auto;
+  border: 2px solid #aac;
+}
+</style>
